e867b2768bd039aaa42ef732df477efc6af4b8ca
larissa-beep <larissa_pentiado@estudante.sesisenai.org.br>
2026-09-21 21:01:34 -0300

    primeiro commit

---
+++ app.js
@@ -0,0 +1,17 @@
+console.log("===============================================");
+console.log("MEU APLICATIVO");
+console.log("===============================================");
+
+console.log("nome: larissa");
+
+
+console.log("cidade: Schroeder");
+
+console.log("atividade de interesse: progrmaçao ");
+
+
+ console.log("quero apredender: croche");
+
+
+console.log("área profissional de interesse : TI");
+console.log("obrigado por me conhecer!");
